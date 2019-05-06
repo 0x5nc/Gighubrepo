7585f115d658e1ee6819235acb2178f2b7e0bd0d
Chakroun Selim <Selim.chakroun@xpo.com>
2019-05-06 18:31:19 +0200

    CreatingGigTable

--- Gighub/Models/ApplicationDbContext.cs
+++ Gighub/Models/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Gighub.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
